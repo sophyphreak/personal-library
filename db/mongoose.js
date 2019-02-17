8e84b1a71ef2c20db67f674d00e7e21baca19b7e
Andrew <arhorn@smcm.edu>
2019-02-18 02:09:27 +0700

    set up error logging for mongodb connections

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,14 +4,16 @@ mongoose.Promise = global.Promise;
 if (process.env.NODE_ENV === 'test') {
   mongoose.connect(`mongodb://localhost:27017/personalLibraryTest`);
 } else {
-  mongoose.connect(
-    `mongodb://${process.env.DB_USER}:${
-      process.env.DB_PASS
-    }@ds139295.mlab.com:39295/personal-library`,
-    {
-      useNewUrlParser: true
-    }
-  );
+  const mongoDB = `mongodb://${process.env.DB_USER}:${
+    process.env.DB_PASS
+  }@ds139295.mlab.com:39295/personal-library`;
+  mongoose.connect(mongoDB, {
+    useNewUrlParser: true
+  });
+
+  const db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 }
 
 module.exports = {
