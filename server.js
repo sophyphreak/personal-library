b175ef86cea561e0a839ad077ed72a3544b762db
Andrew <arhorn@smcm.edu>
2019-02-17 22:27:53 +0700

    added helmet configuration with deceptive poweredby message

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
 
-const apiRoutes = require("./routes/api.js");
-const fccTestingRoutes = require("./routes/fcctesting.js");
-const runner = require("./test-runner");
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 const app = express();
 
-app.use("/public", express.static(process.cwd() + "/public"));
+app.use(
+  helmet({
+    hidePoweredBy: {
+      setTo: 'PHP 4.2.0'
+    }
+  })
+);
 
-app.use(cors({ origin: "*" })); // USED FOR FCC TESTING PURPOSES ONLY!
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.use(cors({ origin: '*' })); // USED FOR FCC TESTING PURPOSES ONLY!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Index page (static HTML)
-app.route("/").get(function(req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+app.route('/').get(function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // For FCC testing purposes
@@ -32,21 +41,21 @@ apiRoutes(app);
 app.use(function(req, res, next) {
   res
     .status(404)
-    .type("text")
-    .send("Not Found");
+    .type('text')
+    .send('Not Found');
 });
 
 // Start our server and tests!
 app.listen(process.env.PORT || 3000, function() {
-  console.log("Listening on port " + process.env.PORT);
-  if (process.env.NODE_ENV === "test") {
-    console.log("Running Tests...");
+  console.log('Listening on port ' + process.env.PORT);
+  if (process.env.NODE_ENV === 'test') {
+    console.log('Running Tests...');
     setTimeout(function() {
       try {
         runner.run();
       } catch (e) {
         const error = e;
-        console.log("Tests are not valid:");
+        console.log('Tests are not valid:');
         console.log(error);
       }
     }, 1500);
